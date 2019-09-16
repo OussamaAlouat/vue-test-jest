5f51f92f884b506c3c2306722637f4d163bf2e62
oussamaAlouat <al317257@uji.es>
2019-09-16 08:35:52 +0200

    Added new component to test it

---
+++ src/components/MessageList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="message in messages" :key="message">
+      {{message}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props: ['messages']
+}
+</script>
+
+<style>
+
+</style>
